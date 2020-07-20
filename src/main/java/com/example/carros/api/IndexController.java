20760da721b16391ea2ff3662e270a2f3007fb09
heliorodri <heliojr97@gmail.com>
2020-07-20 14:05:33 -0300

    First Commit

---
+++ src/main/java/com/example/carros/api/IndexController.java
@@ -0,0 +1,29 @@
+package com.example.carros.api;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    
+    @GetMapping()
+    public String get(){
+        return "Get Spring Boot!";
+    }
+    
+    @PostMapping()
+    public String post() {
+        return "Post Spring Boot!";
+    }
+    
+    @PutMapping()
+    public String put(){
+        return "Put Spring Boot!";
+    }
+    
+    @DeleteMapping()
+    public String delete(){
+        return "Delete Spring Boot!";
+    }
+}
